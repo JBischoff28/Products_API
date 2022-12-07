f999ce4eda3a159ba7c79d4b35fb6c4b5db1d0e4
Joshua Bischoff <joshuabischoff1@gmail.com>
2022-12-06 21:57:05 -0500

    Created CRUD Functionality For The Products API.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,38 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from models import Products
+from serializers import ProductsSerializer
 
+@api_view(['GET', 'POST'])
+def display_products(request):
+
+    if request.method == 'GET':
+        product = Products.objects.all()
+        serializer = ProductsSerializer(product, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def select_product(request, pk):
+    
+    product = Products.objects.get(Products, pk=pk)    
+
+    if request.method == 'GET':
+        serializer = ProductsSerializer(product)
+        return Response(data=serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductsSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save
+        return Response(data=serializer.data)
+        
+    elif request.method == 'DELETE':
+        product.delete
+        return Response(status=status.HTTP_204_NO_CONTENT)
